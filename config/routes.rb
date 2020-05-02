b64c91171490cfc40f8ea8a8c3376ec8f3f2d0a9
Sarah-Ibraheem <sarahmuhammad3377@gmail.com>
2020-05-02 20:51:35 +0200

    lab4

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  post 'authenticate', to: 'authentication#authenticate'
+  resources :items
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
